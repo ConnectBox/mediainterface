1d2f03443dee6f293ead7f19f1d1c7e4aa6c4ba7
Derek Maxson <dmaxson@frontporch.com>
2023-07-19 15:25:51 -0700

    Fix indentations

--- mmiLoader.py
+++ mmiLoader.py
@@ -99,13 +99,13 @@ webpaths = []     # As we find web content, add here so we skip files and folder
 
 # Check for empty directory and write default content if empty
 if (len(os.listdir(mediaDirectory) ) == 0):
-	print("Directory is empty: "+str(mediaDirectory))
-	try:
-			f = open(mediaDirectory + "/connectbox.txt", "w")
-			f.write("<h2>Media Directory Is Empty</h2>Please refer to documentation (placeholder).")
-			f.close()
+  print("Directory is empty: "+str(mediaDirectory))
+  try:
+		f = open(mediaDirectory + "/connectbox.txt", "w")
+		f.write("<h2>Media Directory Is Empty</h2>Please refer to documentation (placeholder).")
+		f.close()
   except:
-  	  pass
+    pass
 
 language = "en"  # By default but it will be overwritten if there are other language directories on the USB
 
